15769d123c2ef62169fcb1b49f4e5ec7b454c0a2
2JongHyeok <gurpaper@naver.com>
2025-04-06 16:05:38 +0900

    cin 이 실패했을때 처리방법

--- CppStudy/CppStudy/study.cpp
+++ CppStudy/CppStudy/study.cpp
@@ -4,9 +4,17 @@ using namespace std;
 
 int main()
 {
-	int c;
-	char d;
+	int c = -1;
+	char d = 'a';
+	char e = 'b';
 	cin >> c >> d;
+	if (cin.fail()) {
+		cout << "입력 실패! 스트림 초기화 중...\n";
+		cin.clear(); // failbit 해제
+		cin.ignore(numeric_limits<streamsize>::max(), '\n'); // 잘못된 입력 버퍼 비우기
+	}
 	cout << c << "\n";
 	cout << d << "\n";
+	cin >> e;
+	cout << e << "\n";
 }
